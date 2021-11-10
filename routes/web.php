2b944cd78742b9cd1e22d04c3a83d153360cae6d
Shogo Okagawa <okagawa1981@gmail.com>
2021-11-10 19:17:58 +0900

    create login/logout

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,20 @@ use App\Http\Controllers\Auth\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => ['guest']], function() {
+    // ログインフォームの表示
+    Route::get('/',[AuthController::class,'showLogin'])->name('login.show');
 
-Route::get('/',[AuthController::class,'showLogin'])->name('showLogin');
+    // ログイン処理
+    Route::post('login',[AuthController::class,'login'])->name('login');
+});
 
-Route::post('login',[AuthController::class,'login'])->name('login');
+Route::group(['middleware' => ['auth']], function() {
+    // ホーム画面
+    Route::get('home', function() {
+        return view('home');
+    })->name('home');
+    // ログアウト
+    Route::post('logout',[AuthController::class,'logout'])->name('logout');
+});
+
